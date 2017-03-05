0a6a1d9266387873e004407f831645e9846fc75e
bentis <ben@tisdall.org.uk>
2017-03-05 23:10:38 +0100

    Appliance stuff.

--- cfn/cfn-appliance.sh
+++ cfn/cfn-appliance.sh
@@ -2,12 +2,19 @@
 
 set -e
 
-ACTION=${1:-update}
+ACTION_RX="^(create|update|delete)$"
+ACTION=$1
+if ! [[ "${ACTION}" =~ ${ACTION_RX} ]]; then
+  echo "ARG1 must match ${ACTION_RX}"
+  exit 1
+fi
 
-ARGS="--template-body file://appliance.yaml --capabilities CAPABILITY_IAM"
+ARGS="--template-body file://appliance.yaml --capabilities CAPABILITY_IAM --parameters"
+ARGS+=" ParameterKey=ServiceUrl,ParameterValue=https://69svcjgnnd.execute-api.eu-west-1.amazonaws.com/v1/claim"
 if [ "${ACTION}" == "delete" ]; then
   ARGS=""
 fi
 
 aws cloudformation "${ACTION}-stack" --stack-name secapp ${ARGS}
+
 aws cloudformation wait "stack-${ACTION}-complete" --stack-name secapp
