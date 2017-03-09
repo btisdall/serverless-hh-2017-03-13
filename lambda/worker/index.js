e8da3d4134c5122627035b17bebf7644372bc5e4
bentis <ben@tisdall.org.uk>
2017-03-09 13:10:15 +0100

    Trailing ws.

--- lambda/worker/index.js
+++ lambda/worker/index.js
@@ -6,7 +6,7 @@ const request = require('request');
 
 const postForm = () => {
   console.log('Calling %s ...', process.env.CLAIM_URL);
-  
+
   const http_done = (err, resp, body) => {
     if (err) {
       console.error(err);
@@ -15,14 +15,14 @@ const postForm = () => {
       console.log('Code: %s', resp.statusCode);
       console.log('Body: %s', body);
     }
-  };   
-  
+  };
+
   request.post(
     process.env.CLAIM_URL,
     { form: { key: 'wibbs', instance_id: Math.random().toString() } },
     http_done
   );
-};  
+};
 
 exports.handler = (event, context, callback) => {
   postForm();
