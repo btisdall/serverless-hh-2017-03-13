4f9d10da1924ac979a73a86f0397aecdb39707a9
bentis <ben@tisdall.org.uk>
2017-03-09 23:21:54 +0100

    ASG->API direct working.

--- lambda/worker/index.js
+++ lambda/worker/index.js
@@ -1,29 +1,43 @@
 'use strict';
 console.log('Loading function');
 
-const AWS = require('aws-sdk');
 const request = require('request');
+const AWS = require('aws-sdk');
+const sqs = new AWS.SQS();
 
-const postForm = () => {
-  console.log('Calling %s ...', process.env.CLAIM_URL);
+exports.handler = (event, context, callback) => {
+  var instanceId = JSON.parse(JSON.parse(event.Body).Message).EC2InstanceId;
+  console.log('HANDLE: %s, INSTANCE_ID: %s', event.ReceiptHandle, instanceId);
 
-  const http_done = (err, resp, body) => {
-    if (err) {
-      console.error(err);
-    }
-    else {
-      console.log('Code: %s', resp.statusCode);
-      console.log('Body: %s', body);
+  var params = {
+    form: {
+      key: new Date().toISOString(),
+      instance_id: instanceId,
     }
   };
 
   request.post(
-    process.env.CLAIM_URL,
-    { form: { key: 'wibbs', instance_id: Math.random().toString() } },
-    http_done
+    process.env.CLAIM_URL, params, function(err, resp, body) {
+      if (err) {
+        console.error(err);
+        callback({ msg: 'Something wrong went' });
+      }
+      else {
+        console.log('code: %s', resp.statusCode);
+        console.log('body: %s', body);
+        params = {
+          QueueUrl: process.env.QUEUE_URL,
+          ReceiptHandle: event.ReceiptHandle,
+        };
+        sqs.deleteMessage(params, function(err, data) {
+          if (err) {
+            console.log(err, err.stack);
+          }
+          else {
+            console.log(data);
+          }
+        });
+      }
+    }
   );
-};
-
-exports.handler = (event, context, callback) => {
-  postForm();
 };
